391c0e4a29ed12ffae4a3e28d5a7a0defdd2731e
yooju00 <yooju00@naver.com>
2024-11-10 22:35:33 +0900

    refactor(src): 초기화 로직 분리

--- src/App.js
+++ src/App.js
@@ -8,28 +8,44 @@ import Membership from './domain/Membership.js';
 import ValidationService from "./service/ValidationService.js";
 import ShoppingService from './service/ShoppingService.js';
 import OrderService from "./service/OrderService.js";
+import InventoryService from "./service/InventoryService.js";
 
 class App {
   constructor() {
+    this.initializeServices();
+    this.initializeViews();
+    this.initializeBusinessLogic();
+  }
+
+  initializeServices() {
     this.productService = new ProductService();
     this.promotionService = new PromotionService();
     this.membership = new Membership();
+    this.inventoryService = new InventoryService();
     this.discountService = new DiscountService(this.promotionService, this.membership);
-    this.paymentService = new PaymentService(
-        this.productService,
-        this.promotionService,
-        this.discountService
-    );
+  }
+
+  initializeViews() {
     this.inputView = new InputView();
     this.outputView = new OutputView();
     this.validationService = new ValidationService(this.outputView);
+  }
+
+  initializeBusinessLogic() {
+    this.paymentService = new PaymentService(
+        this.productService,
+        this.promotionService,
+        this.discountService,
+        this.inventoryService
+    );
 
     this.orderService = new OrderService(
         this.productService,
         this.paymentService,
         this.inputView,
         this.outputView,
-        this.validationService
+        this.validationService,
+        this.inventoryService
     );
 
     this.shoppingService = new ShoppingService(
