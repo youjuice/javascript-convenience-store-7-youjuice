1423721406ff935a8d0fa993739552f073ec5b7f
yooju00 <yooju00@naver.com>
2024-11-11 22:14:34 +0900

    style(constants): 중복 상수 RECEIPT 제거

--- src/constants/config.js
+++ src/constants/config.js
@@ -6,11 +6,6 @@ export const CONFIG = {
     PROMOTION: {
         FREE_QUANTITY: 1
     },
-    RECEIPT: {
-        HEADER: '===========W 편의점=============',
-        GIFT_HEADER: '===========증\t정=============',
-        FOOTER: '=============================='
-    },
     FILE: {
         PRODUCTS: 'public/products.md',
         PROMOTIONS: 'public/promotions.md'
